4b3672ec907cebaed59958a72929ca49b5440b6c
okTom Dev <tom@barsby.org>
2021-11-29 15:00:51 +0000

    Release: bs-jobcentreplus

---
+++ bs-jobcentreplus/README.md
@@ -0,0 +1,8 @@
+Thanks for downloading our Job Centre Plus..
+
+To install simply drag the file inside your server-data\resources folder and write start bs-jobcentreplus 
+in your server.cfg
+
+Credits to VerpiMoDz Maps.
+
+-- © British Studios 2021
---
+++ bs-jobcentreplus/bs-jobcentreplus/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'British Studios & VerpiMoDz Maps'
+description 'British Studios Job Centre Plus'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+-- Credits to VerpiMoDz Maps. Join there discord @ https://discord.gg/dmPpZaqAjY
